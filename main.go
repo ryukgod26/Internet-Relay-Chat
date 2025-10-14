a20d1184dcd6061de333b08a9c0a31a3f8fb0d9c
raviwhitehat <ekravi26@gmail.com>
2025-10-14 22:48:40 +0530

    trying to learn Go by building a Flight Ticket System

--- main.go
+++ main.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	fmt.Print("Test")
+	var totalTickets uint16
+	var userTickets uint16
+	var firstName string
+	var lastName string
+	var email string
+	totalTickets = 20
+	flightName := "Flight"
+	var bookings = []string{"Armin","Eren","Mikasa"}
+	fmt.Printf("We have %d Tickets.\n", totalTickets)
+	fmt.Printf("Type:%T\n", totalTickets)
+	fmt.Printf("Flight Name is %s\n", flightName)
+	fmt.Println("Current Booking ",bookings,".")
+for{
+	fmt.Print("Enter Your First Name:")
+	fmt.Scan(&firstName)
+
+	fmt.Print("Enter Your Last Name:")
+	fmt.Scan(&lastName)
+
+	if !(len(firstName) >= 2) && !(len(lastName) >=2){
+		fmt.Println("First Name and Last Name Must Have More than 2 characters.")
+		continue
+	}
+
+	fmt.Print("Enter You E-mail:")
+	fmt.Scan(&email)
+
+	fmt.Print("Enter Number of Tickets:")
+	fmt.Scan(&userTickets)
+
+	if userTickets > totalTickets{
+		fmt.Printf("We only have %d tickets left. You cannot Book %d Tickets.",totalTickets,userTickets)
+		continue
+	}
+
+	totalTickets -= userTickets
+
+	bookings = append(bookings ,firstName + " " +lastName)
+
+	firstNames := []string{}
+	for _,booking := range bookings {
+		names := strings.Fields(booking)
+		firstNames = append(firstNames,names[0])
+	}
+	if totalTickets == 0{
+		fmt.Println("Sorry All the tickets are Booked. You cannot Book More Tickets.")
+		break
+	}
+
+	fmt.Printf("Username: %s\n", firstName+" " + lastName)
+	fmt.Printf("User Tickets: %d\n", userTickets)
+	fmt.Println("Current Booking ",firstNames,".")
+}
 }
