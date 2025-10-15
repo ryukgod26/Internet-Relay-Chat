225d6124cfcef3f8b62c92dc361d4e3696716b6e
raviwhitehat <ekravi26@gmail.com>
2025-10-15 18:01:06 +0530

    learn how to create effectively in Go.

--- main.go
+++ main.go
@@ -19,44 +19,63 @@ func main() {
 	fmt.Printf("Flight Name is %s\n", flightName)
 	fmt.Println("Current Booking ", bookings, ".")
 	for {
-		fmt.Print("Enter Your First Name:")
-		fmt.Scan(&firstName)
-
-		fmt.Print("Enter Your Last Name:")
-		fmt.Scan(&lastName)
-
-		fmt.Print("Enter You E-mail:")
-		fmt.Scan(&email)
-
+		
+		userTickets,firstName,lastName,email = getUserInput()
 		if !(len(firstName) >= 2) && !(len(lastName) >= 2) && !(strings.Contains(email, "@")) {
 			fmt.Println("First Name and Last Name Must Have More than 2 characters and Email Must be Valid.")
 			continue
 		}
 
-		fmt.Print("Enter Number of Tickets:")
-		fmt.Scan(&userTickets)
+
 
 		if userTickets > totalTickets {
 			fmt.Printf("We only have %d tickets left. You cannot Book %d Tickets.", totalTickets, userTickets)
 			continue
 		}
-
+		bookings = append(bookings, firstName+" "+lastName)
 		totalTickets -= userTickets
 
-		bookings = append(bookings, firstName+" "+lastName)
-
-		firstNames := []string{}
-		for _, booking := range bookings {
-			names := strings.Fields(booking)
-			firstNames = append(firstNames, names[0])
-		}
-		if totalTickets == 0 {
+		 firstNames := getFirstNames(bookings)
+			if totalTickets == 0 {
 			fmt.Println("Sorry All the tickets are Booked. You cannot Book More Tickets.")
 			break
 		}
 
 		fmt.Printf("Username: %s\n", firstName+" "+lastName)
 		fmt.Printf("User Tickets: %d\n", userTickets)
-		fmt.Println("Current Booking ", firstNames, ".")
+					fmt.Println("Current Booking ", firstNames, ".")
+
 	}
+}
+
+func getUserInput() (uint16,string,string,string) {
+	var userTickets uint16
+	var firstName string
+	var lastName string
+	var email string
+	fmt.Print("Enter Your First Name:")
+	fmt.Scan(&firstName)
+
+	fmt.Print("Enter Your Last Name:")
+	fmt.Scan(&lastName)
+
+	fmt.Print("Enter You E-mail:")
+	fmt.Scan(&email)
+
+	fmt.Print("Enter Number of Tickets:")
+	fmt.Scan(&userTickets)
+
+	return userTickets,firstName,lastName,email
+}
+
+func getFirstNames(bookings[] string) []string{
+
+
+		firstNames := []string{}
+		for _, booking := range bookings {
+			names := strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+
+		return firstNames
 }
