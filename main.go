40e595f5f19f787364dbcc9c3979893325594394
raviwhitehat <ekravi26@gmail.com>
2025-10-20 14:22:25 +0530

    Craeted A basic TUI for Questions. Currently the Yui is not listening to Answers But will that in next commit.

--- main.go
+++ main.go
@@ -7,10 +7,96 @@ import (
 	"log"
 	"os"
 	"strings"
-	"tui/tui"
+
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
-type model struct{}
+type Styles struct {
+	BorderColor lipgloss.Color
+	InputField  lipgloss.Style
+}
+
+type model struct {
+	questions   []string
+	width       int
+	height      int
+	index       int
+	answerField textinput.Model
+	styles      *Styles
+}
+
+func DefaultStyles() *Styles {
+	s := new(Styles)
+	s.BorderColor = lipgloss.Color("36")
+
+	s.InputField = lipgloss.NewStyle().
+		BorderForeground(s.BorderColor).
+		BorderStyle(lipgloss.NormalBorder()).
+		Padding(1).
+		Width(80)
+
+	return s
+}
+
+func New(questions []string) *model {
+	styles := DefaultStyles()
+	answerField := textinput.New()
+	answerField.Placeholder = "Enter Your Answer Here"
+	answerField.Focus()         // ensure placeholder / cursor appears
+	answerField.CharLimit = 512 // allow longer input
+	answerField.Width = 60      // ensure
+	return &model{questions: questions, answerField: answerField, styles: styles}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	case tea.WindowSizeMsg:
+		m.width = msg.Width
+		m.height = msg.Height
+
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c":
+			return m, tea.Quit
+		case "enter":
+			if m.index < 5{
+			m.index ++
+			return m,nil
+			}else{
+			m.answerField.SetValue("Done!")
+			return m,nil
+			}
+		}
+
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	if m.width == 0 {
+		return "loading...."
+	}
+	return lipgloss.Place(
+		m.width,
+		m.height,
+		lipgloss.Center,
+		lipgloss.Center,
+		lipgloss.JoinVertical(
+		lipgloss.Center,
+		m.questions[m.index],
+		m.styles.InputField.Render(m.answerField.View()),
+	),
+	)
+	
+	
+}
 
 const (
 	domain = "irc.oftc.net"
@@ -21,13 +107,23 @@ const (
 
 func main() {
 
-	f, err := tui.tea.LogToFile("debug.log", "debug")
-	tui.Handle_error(err)
+	questions := []string{
+		"What is Your Name?",
+		"What is Your Username?",
+		"What is Your Nickname?",
+		"What is the Domain of the Server You Want to connect to?",
+		"What is the Serevr Port?",
+		"What is the Channnel name You wnat to enter?"}
+
+	m := New(questions)
+
+	f, err := tea.LogToFile("debug.log", "debug")
+	irc.Handle_error(err)
 	defer f.Close()
 
-	p := tui.tea.NewProgram(model{}, tui.tea.WithAltScreen())
+	p := tea.NewProgram(m, tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
-		tui.Handle_error(err)
+		irc.Handle_error(err)
 	}
 
 	client := irc.Init(domain, port, "1223", user, nick)
